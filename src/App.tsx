d3d7de256a40df3b6b2356f2720ca597721a46b9
ahuiru <zhuangerlangyou@gmail.com>
2022-10-23 15:39:15 +0900

    day click

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,29 @@
-import React from "react";
+import React, { useCallback } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import allLocales from "@fullcalendar/core/locales-all";
+import interactionPlugin, {DateClickArg} from "@fullcalendar/interaction";
 import "./App.css";
 
+const thisMonth = () => {
+  const today = new Date();
+  return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(
+    2,
+    "0"
+  )}`;
+};
+
 function App() {
-  const thisMonth = () => {
-    const today = new Date();
-    return `${today.getFullYear()}-${String(today.getMonth() + 1).padStart(2, '0')
-    }`;
-  };
+
+  const handleDateClick = useCallback((arg: DateClickArg) => {
+    console.log(arg)
+    alert(arg.dateStr);
+  }, []);
 
   return (
     <div className="App">
       <FullCalendar
-        plugins={[dayGridPlugin]}
+        plugins={[dayGridPlugin, interactionPlugin]}
         initialView="dayGridMonth"
         locales={allLocales}
         locale="ja"
@@ -22,6 +31,7 @@ function App() {
           { title: "event1", date: `${thisMonth()}-01` },
           { title: "event2", date: `${thisMonth()}-02` },
         ]}
+        dateClick={handleDateClick}
       />
     </div>
   );
