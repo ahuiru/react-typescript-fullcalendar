d12c6d3a89b44d0f1ea5597c9c033dc9337c3199
ahuiru <zhuangerlangyou@gmail.com>
2022-10-24 23:33:32 +0900

    List

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React, { useCallback } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
 import allLocales from "@fullcalendar/core/locales-all";
-import interactionPlugin, {DateClickArg} from "@fullcalendar/interaction";
+import interactionPlugin, { DateClickArg } from "@fullcalendar/interaction";
+import listPlugin from "@fullcalendar/list";
 import "./App.css";
 
 const thisMonth = () => {
@@ -16,21 +18,35 @@ const thisMonth = () => {
 function App() {
 
   const handleDateClick = useCallback((arg: DateClickArg) => {
-    console.log(arg)
-    alert(arg.dateStr);
+    // console.log(arg);
+    // alert(info.event.title);
   }, []);
+
 
   return (
     <div className="App">
       <FullCalendar
-        plugins={[dayGridPlugin, interactionPlugin]}
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin]}
         initialView="dayGridMonth"
         locales={allLocales}
         locale="ja"
+        eventClick={
+          function (arg) {
+            console.log(arg.event)
+            alert([arg.event.title, arg.event.startStr])
+            // alert(arg.event.start)
+          }
+        }
         events={[
           { title: "event1", date: `${thisMonth()}-01` },
           { title: "event2", date: `${thisMonth()}-02` },
+          { title: "basket", date: `${thisMonth()}-08` },
         ]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,listWeek",
+        }}
         dateClick={handleDateClick}
       />
     </div>
